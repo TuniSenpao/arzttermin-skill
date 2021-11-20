59bc63d5b27b83fa6e0ce0147ed36fa6a29d707c
TuniSenpao <robert-liefke@gmx.de>
2021-11-20 12:14:27 +0100

    ask for second input when he couldnt understand the date

--- __init__.py
+++ __init__.py
@@ -310,15 +310,19 @@ class ArztterminSkill(MycroftSkill):
         date = None
         date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=10)
         months = ['januar', 'februar','märz', 'april', 'mai', 'juni', 'juli', 'august', 'september','oktober','november','dezember']
-        days = ['erster', 'ersten', 'zweiter','zweiten' ,'dritter', 'dritten','3.','4.','5.','6.','7.','8.','9.','10.','11.','12.','13.','14.','15',
-                '16.','17.','18.','19.','20.','21.','22.','23.','24.','25.','26.','27','28.','29.','30.','31.']
+        days = ['erster', 'ersten', 'zweiter','zweiten' ,'dritter', 'dritten','3','4','5','6','7','8','9','10','11','12','13','14','15',
+                '16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
         day = [d for d in days if(d in date_response)]
         month = [m for m in months if(m in date_response)]
 
         if (bool(day) and bool(month)):
-            date = day[-1] + ' ' + month[-1]
+            date = day[-1] + '. ' + month[-1]
         else:
+            # TODO: besser nach wiederholung fragen
             date_response = self.get_response('ParticularDate', on_fail='wait.for.answer', num_retries=10)
+            day = [d for d in days if(d in date_response)]
+            month = [m for m in months if(m in date_response)]
+            date = day[-1] + ' ' + month[-1]
 
         """
         date, _ = extract_datetime(date_response, lang=self.lang)
